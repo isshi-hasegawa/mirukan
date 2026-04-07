4732f833aa7d26438441db020d54b6fbf4ecbeba
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2026-04-07 23:24:32 +0900

    fix: ビルド警告の解消

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 750,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
